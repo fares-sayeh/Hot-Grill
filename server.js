e59bd73eeda13ca5ca519951ade13608556212af
Fares <fares.sayeh.20.12@gmail.com>
2026-06-02 15:53:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.post("/send", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("server running on port " + PORT);
 });
